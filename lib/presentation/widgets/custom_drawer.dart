ace5b156ce2b68b7870d1ca4c59c55b0bf45fc0b
Saepudin <agung.saepudin@ist.id>
2022-04-21 22:34:58 +0700

    add animation drawer

---
+++ lib/presentation/widgets/custom_drawer.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+import '../pages/about_page.dart';
+import '../pages/watchlist_movies_page.dart';
+
+class CustomDrawer extends StatefulWidget {
+  final Widget content;
+
+  const CustomDrawer({Key? key, required this.content}) : super(key: key);
+
+  @override
+  State<CustomDrawer> createState() => _CustomDrawerState();
+}
+
+class _CustomDrawerState extends State<CustomDrawer>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  void toggle() => _animationController.isDismissed
+      ? _animationController.forward()
+      : _animationController.reverse();
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 250));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: toggle,
+      child: AnimatedBuilder(
+          animation: _animationController,
+          builder: (context, child) {
+            double slide = 240.0 * _animationController.value;
+            double scale = 1 - (_animationController.value * 0.3);
+
+            return Stack(children: [
+              Container(
+                child: Column(
+                  children: [
+                    UserAccountsDrawerHeader(
+                      currentAccountPicture: CircleAvatar(
+                        backgroundImage: AssetImage('assets/circle-g.png'),
+                      ),
+                      accountName: Text('Ditonton'),
+                      accountEmail: Text('ditonton@dicoding.com'),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.movie),
+                      title: Text('Movies'),
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.save_alt),
+                      title: Text('Watchlist'),
+                      onTap: () {
+                        Navigator.pushNamed(
+                            context, WatchlistMoviesPage.ROUTE_NAME);
+                      },
+                    ),
+                    ListTile(
+                      onTap: () {
+                        Navigator.pushNamed(context, AboutPage.ROUTE_NAME);
+                      },
+                      leading: Icon(Icons.info_outline),
+                      title: Text('About'),
+                    ),
+                  ],
+                ),
+              ),
+              Transform(
+                transform: Matrix4.identity()
+                  ..translate(slide)
+                  ..scale(scale),
+                alignment: Alignment.centerLeft,
+                child: widget.content,
+              ),
+            ]);
+          }),
+    );
+  }
+}
